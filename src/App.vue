8e1051b33115ca3d9d3a66c6000dd11bfb28ff88
E. M. Greeff <893946+modster@users.noreply.github.com>
2022-02-11 06:25:50 -0700

    my first overlay

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,51 @@
 <template>
-<!-- TradingVueJs 101 (example from 'Getting Started' ) -->
+  <!-- TradingVueJs 101 (example from 'Getting Started' ) !-->
 
-<trading-vue :data="chart" :width="this.width" :height="this.height"
+  <trading-vue
+    :data="chart"
+    :width="this.width"
+    :height="this.height"
+    :overlays="overlays"
     :color-back="colors.colorBack"
     :color-grid="colors.colorGrid"
-    :color-text="colors.colorText">
-</trading-vue>
+    :color-text="colors.colorText"
+  >
+  </trading-vue>
 </template>
 
 <script>
-import TradingVue from 'trading-vue-js'
-import Data from '../data/data.json'
+import TradingVue from "trading-vue-js";
+import Data from "../data/data.json";
+import Grin from "./Grin.js";
+import PerfectTrades from "./PerfectTrades.js";
 
 export default {
-    name: 'app',
-    components: { TradingVue },
-    methods: {
-        onResize(event) {
-            this.width = window.innerWidth
-            this.height = window.innerHeight
-        }
+  name: "app",
+  components: { TradingVue },
+  methods: {
+    onResize(event) {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
     },
-    mounted() {
-        window.addEventListener('resize', this.onResize)
-    },
-    beforeDestroy() {
-        window.removeEventListener('resize', this.onResize)
-    },
-    data() {
-        return {
-            chart: Data,
-            width: window.innerWidth,
-            height: window.innerHeight,
-            colors: {
-                colorBack: '#fff',
-                colorGrid: '#eee',
-                colorText: '#333',
-            }
-        }
-    }
-}
+  },
+  mounted() {
+    window.addEventListener("resize", this.onResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  data() {
+    return {
+      chart: Data,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      colors: {
+        colorBack: "#fff",
+        colorGrid: "#eee",
+        colorText: "#333",
+      },
+      overlays: [PerfectTrades],
+    };
+  },
+};
 </script>
